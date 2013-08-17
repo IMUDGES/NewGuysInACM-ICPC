2c8c8b9e095de1fccea69eee64910fbba04502b3
石博天 <friskit.china@gmail.com>
2013-08-17 09:03:58 +0800

    新增了一个代码文件夹 似乎可以从latex的listing中直接导入代码，但是我用了注释功能，可能会跟代码有冲突，再研究一下~

---
+++ LaTeX_Workspace/codes/1-1/1-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char *name;
+
+	cout<<"  Welcome to IMUDGES"<<endl;
+	cout<<"  Who are you?"<<endl;
+	cout<<">>"<<endl;
+	cin>>name;
+	cout<<"  Hello, "<<name<<"!"<<endl;
+}
